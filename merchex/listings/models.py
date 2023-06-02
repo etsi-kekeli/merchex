7f3d1cd0d0675fd3e670d9e96c3df94d38c84957
Samuel <kumasamuel94@gmail.com>
2023-06-02 05:30:56 +0100

    Add attributes to the models and add style to the templates

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,13 +1,40 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from datetime import date
+
+def get_current_year():
+    return date.today().year
 
 class Band(models.Model):
-    name = models.fields.CharField(max_length=100)
+    
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+        AFRO_BEAT = 'AB'
+        RNB = 'RNB'
 
-    def __str__(self) -> str:
-        return f"Band: {self.name}"
+    name = models.fields.CharField(max_length=100)
+    genre = models.CharField(max_length=5, choices=Genre.choices, default=Genre.RNB)
+    biography = models.CharField(max_length=1000, null=True)
+    year_formed = models.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(get_current_year)],
+        default=get_current_year
+                    )
+    active = models.BooleanField(default=True)
+    official_homepage = models.URLField(null=True, blank=True)
+
     
 class Listing(models.Model):
+    class Type(models.TextChoices):
+        RECORDS = 'REC'
+        CLOTHES = 'CLO'
+        POSTERS = 'POST'
+        MISCELLANEOUS = 'MISC'
+
     title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200, default='')
+    sold = models.BooleanField(default=False)
+    year = models.IntegerField(null=True, validators=[MaxValueValidator(get_current_year)])
+    type = models.CharField(max_length=5, choices=Type.choices, default=Type.MISCELLANEOUS)
     
-    def __str__(self) -> str:
-        return f"Band: {self.title}"
